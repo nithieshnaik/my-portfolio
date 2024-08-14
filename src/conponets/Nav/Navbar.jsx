63985f83bae0a489999b45078cc9a0e7f42348b9
Nithiesh Naik <nithieshnaik7@gmail.com>
2024-08-14 13:44:58 +0530

    change

--- src/conponets/Nav/Navbar.jsx
+++ src/conponets/Nav/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     const [ menuOpen ,setMenuOpen] = useState(false) ;
   return (
    <nav  className={Styles.navbar}>
-    <a  className ={Styles.title} href='/'>Portfolio</a> 
+    <a  className ={Styles.title} href='/'> My Portfolio</a> 
     <div className={Styles.menu}> 
             <img className={Styles.menuBtn} src={ 
                 menuOpen 
